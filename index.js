0556f768420c89544e4bc7bbd21effa8c5c7ea6c
Ram <ram@Rams-MacBook-Pro.local>
2025-03-15 00:21:15 +0530

    fix a typo error while sending response to client

--- index.js
+++ index.js
@@ -13,28 +13,43 @@ app.post("/todo", async (req, res) => {
   const body = req.body;
   const parsedPayload = createTodo.safeParse(body);
   if (!parsedPayload.success) {
-    res.statusCode(411).json({ message: "You send the wrong inputs" });
+    res.status(411).json({ message: "You send the wrong inputs" });
     return;
   }
 
   await todo.create({
     title: req.body.title,
     description: req.body.description,
+    completed: false,
   });
 
-  res.statusCode(200).json({ message: "todo created" });
+  res.status(200).json({ message: "todo created" });
 });
 
-app.get("/todos", (req, res) => {});
+app.get("/todos", async (req, res) => {
+  const todos = await todo.find({});
+  res.status(200).json({ todos });
+});
 
-app.put("/completed", (req, res) => {
+app.put("/completed", async (req, res) => {
   const body = req.body;
   const parsedPayload = updateTodo.safeParse(body);
 
   if (!parsedPayload.success) {
-    res.statusCode(411).json({ message: "Invalid Inputs" });
+    res.status(411).json({ message: "Invalid Inputs" });
     return;
   }
+
+  await todo.update(
+    {
+      _id: req.body.id,
+    },
+    {
+      completed: true,
+    }
+  );
+
+  res.status(200).json({ message: "Update Success" });
 });
 
 app.listen(PORT, (err) => {
